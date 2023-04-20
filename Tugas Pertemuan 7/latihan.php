2a53ed792bc44fffd287d68c9c07bc200f0608fd
Riska Rismaya <riskarismaya018@gmail.com>
2023-04-20 12:40:00 +0700

    menambahkan dua file di Tugas Pertemuan 7

---
+++ Tugas Pertemuan 7/latihan.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="10" cellpadding="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NIM</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td><img src="" img/Riska Rismaya.jpg></td>
+      <td>A22100116</td>
+      <td>Riska Rismaya</td>
+      <td>riskariskamya028@gmail.com</td>
+      <td>Teknik Informatika</td>
+      <td>
+        <a href="">Ubah</a> | <a href="">Hapus</a>
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
